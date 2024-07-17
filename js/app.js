111708a99b38551b98b8787ece2a97e40170f309
Cristian Loaiza Huallpa <49375434+cristianloa@users.noreply.github.com>
2024-07-17 17:47:46 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const stationsList = [
-      { name: 'Estación 1', url: 'https://mdstrm.com/audio/5fab0687bcd6c2389ee9480c/icecast.audio' },
-      { name: 'Estación 2', url: 'https://mdstrm.com/audio/5fad731fcf097a068af3c8f7/icecast.audio' },
-      { name: 'Estación 3', url: 'http://example.com/stream3' },
-      // Añade más estaciones aquí
-    ];
-  
-    const stationsContainer = document.getElementById('stations-list');
-    const stationNameElement = document.getElementById('station-name');
-    const audioSourceElement = document.getElementById('audio-source');
-    const audioPlayerElement = document.getElementById('audio-player');
-  
-    stationsList.forEach(station => {
-      const stationElement = document.createElement('div');
-      stationElement.innerHTML = `
-        <h2>${station.name}</h2>
-        <button onclick="playStation('${station.name}', '${station.url}')">Escuchar</button>
-      `;
-      stationsContainer.appendChild(stationElement);
-    });
-  
-    window.playStation = function(name, url) {
-      stationNameElement.innerText = name;
-      audioSourceElement.src = url;
-      audioPlayerElement.load();
-      audioPlayerElement.play();
-    };
+  const stationsList = [
+    { name: 'Radio Oxígeno', url: 'https://mdstrm.com/audio/5fab0687bcd6c2389ee9480c/icecast.audio' },
+    { name: 'Radio Felicidad', url: 'https://mdstrm.com/audio/5fad731fcf097a068af3c8f7/icecast.audio' },
+    { name: 'Radio Oxígeno', url: 'https://mdstrm.com/audio/5fab0687bcd6c2389ee9480c/icecast.audio' },
+    { name: 'Radio Felicidad', url: 'https://mdstrm.com/audio/5fad731fcf097a068af3c8f7/icecast.audio' },
+    { name: 'Radio Oxígeno', url: 'https://mdstrm.com/audio/5fab0687bcd6c2389ee9480c/icecast.audio' },
+    { name: 'Radio Felicidad', url: 'https://mdstrm.com/audio/5fad731fcf097a068af3c8f7/icecast.audio' },
+    { name: 'Radio Oxígeno', url: 'https://mdstrm.com/audio/5fab0687bcd6c2389ee9480c/icecast.audio' },
+    { name: 'Radio Felicidad', url: 'https://mdstrm.com/audio/5fad731fcf097a068af3c8f7/icecast.audio' },
+    
+  ];
+
+  const stationsContainer = document.getElementById('stations-list');
+  const stationNameElement = document.getElementById('station-name');
+  const audioSourceElement = document.getElementById('audio-source');
+  const audioPlayerElement = document.getElementById('audio-player');
+
+  stationsList.forEach(station => {
+    const stationElement = document.createElement('div');
+    stationElement.innerHTML = `
+      <h3>${station.name}</h3>
+      <button onclick="playStation('${station.name}', '${station.url}')">Escuchar</button>
+    `;
+    stationsContainer.appendChild(stationElement);
   });
-  
+
+  window.playStation = function(name, url) {
+    stationNameElement.innerText = name;
+    audioSourceElement.src = url;
+    audioPlayerElement.load();
+    audioPlayerElement.play();
+  };
+});
